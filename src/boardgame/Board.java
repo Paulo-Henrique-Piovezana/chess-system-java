776b60cd0e23b009ebe4dfff456fc0769942408c
Mona Lisa <paulopiovezana@gmail.com>
2025-06-24 11:22:15 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -4,12 +4,12 @@ public class Board {
 
 	private int rows;
 	private int colums;
-	private Piece[][] piece;
+	private Piece[][] pieces;
 
 	public Board(int rows, int colums) {
 		this.rows = rows;
 		this.colums = colums;
-		piece = new Piece[rows][colums];
+		pieces = new Piece[rows][colums];
 	}
 
 	public int getRows() {
@@ -28,5 +28,12 @@ public class Board {
 		this.colums = colums;
 	}
 
+	public Piece piece(int row, int colum) {
+		return pieces[row][colum];
+	}
+	
+	public Piece piece (Position position) {
+		return pieces [position.getRow()] [position.getColum()];
+	}
 	
 }
